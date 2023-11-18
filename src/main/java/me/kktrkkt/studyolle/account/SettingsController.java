467628f902b6039bc5d0a396e9bd66b3a27b5eef
KKTRKKT <paulhana6006@gmail.com>
2023-11-18 12:14:08 +0900

    add settings profile form

--- src/main/java/me/kktrkkt/studyolle/account/SettingsController.java
+++ src/main/java/me/kktrkkt/studyolle/account/SettingsController.java
@@ -3,12 +3,19 @@ package me.kktrkkt.studyolle.account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class SettingsController {
 
     private static final String PROFILE_UPDATE_FORM = "settings/profileUpdateForm";
+
+    @ModelAttribute(name = "account")
+    public Account currentUser(@CurrentUser Account account){
+        return account;
+    }
 
     @GetMapping("/settings/profile")
     public String profileUpdateForm() {
