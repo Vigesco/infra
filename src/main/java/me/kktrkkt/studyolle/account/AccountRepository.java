44cf2604f601c4d75fb933bce20454225442bb8d
r2soft <paulhana6006@gmail.com>
2023-12-01 16:40:34 +0900

    Update repository's transactional readonly

--- src/main/java/me/kktrkkt/studyolle/account/AccountRepository.java
+++ src/main/java/me/kktrkkt/studyolle/account/AccountRepository.java
@@ -4,9 +4,11 @@ import me.kktrkkt.studyolle.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByEmail(String email);
