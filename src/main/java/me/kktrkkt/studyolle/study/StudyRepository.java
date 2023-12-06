7eefe2dffd4651304c54729095fc3bd9f60da51d
r2soft <paulhana6006@gmail.com>
2023-12-06 13:44:15 +0900

    Set study repository readonly

--- src/main/java/me/kktrkkt/studyolle/study/StudyRepository.java
+++ src/main/java/me/kktrkkt/studyolle/study/StudyRepository.java
@@ -1,9 +1,11 @@
 package me.kktrkkt.studyolle.study;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface StudyRepository extends JpaRepository<Study, Long> {
     Optional<Study> findByPath(String path);
 }
