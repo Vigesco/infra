8efe111498721b9f367b9460c19ea9b7c504eaed
r2soft <paulhana6006@gmail.com>
2023-12-18 13:55:15 +0900

    Add event view

--- src/main/java/me/kktrkkt/studyolle/event/EventController.java
+++ src/main/java/me/kktrkkt/studyolle/event/EventController.java
@@ -5,6 +5,7 @@ import me.kktrkkt.studyolle.account.CurrentUser;
 import me.kktrkkt.studyolle.account.entity.Account;
 import me.kktrkkt.studyolle.study.Study;
 import me.kktrkkt.studyolle.study.StudyService;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -21,9 +22,11 @@ public class EventController {
     static final String NEW_EVENT_URL = BASE_URL + "/new-event";
     static final String NEW_EVENT_VIEW = "event/newEventForm";
     static final String EVENT_URL = BASE_URL + "/event/{id}";
+    static final String EVENT_VIEW = "event/view";
 
     private final StudyService studyService;
     private final EventService eventService;
+    private final EventRepository events;
 
     @InitBinder("eventForm")
     public void validEventForm(WebDataBinder dataBinder){
@@ -50,5 +53,17 @@ public class EventController {
         }
         Event newEvent = eventService.create(account, eventForm, study);
         return "redirect:" + EVENT_URL.replace("{id}", String.valueOf(newEvent.getId()));
+    }
+
+    @GetMapping(EVENT_URL)
+    public String eventView(@PathVariable String path, @PathVariable Long id,
+                            @CurrentUser Account account, Model model) {
+        Study study = studyService.getStudyToUpdateMembers(path);
+        if(!study.isMember(account) && !study.isManager(account)){
+            throw new AccessDeniedException("해당 모임에 접근할 권한이 없습니다.");
+        }
+        model.addAttribute(study);
+        model.addAttribute(events.findById(id).orElseThrow());
+        return EVENT_VIEW;
     }
 }
