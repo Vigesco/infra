80796ac9d9ada3f154d8f82364a324db4bccc25b
r2soft <paulhana6006@gmail.com>
2024-01-05 17:01:46 +0900

    Add notification domain

---
+++ src/main/java/me/kktrkkt/studyolle/modules/notification/Notification.java
@@ -0,0 +1,34 @@
+package me.kktrkkt.studyolle.modules.notification;
+
+import lombok.*;
+import me.kktrkkt.studyolle.infra.entity.BaseEntity;
+import me.kktrkkt.studyolle.modules.account.entity.Account;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Notification extends BaseEntity<Notification> {
+
+    private String link;
+
+    private String title;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account to;
+
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.ORDINAL)
+    private NotificationType notificationType;
+}
