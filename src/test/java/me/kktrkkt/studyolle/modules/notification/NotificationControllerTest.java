0b420cdb0b481ce93610f369a2cce1e573fac0d2
r2soft <paulhana6006@gmail.com>
2024-01-12 15:53:13 +0900

    Add notification view test

---
+++ src/test/java/me/kktrkkt/studyolle/modules/notification/NotificationControllerTest.java
@@ -0,0 +1,77 @@
+package me.kktrkkt.studyolle.modules.notification;
+
+import me.kktrkkt.studyolle.infra.MockMvcTest;
+import me.kktrkkt.studyolle.modules.account.AccountRepository;
+import me.kktrkkt.studyolle.modules.account.WithAccount;
+import me.kktrkkt.studyolle.modules.account.entity.Account;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@MockMvcTest
+@Transactional
+class NotificationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private NotificationRepository notifications;
+
+    @Autowired
+    private NotificationFactory notificationFactory;
+
+    @Autowired
+    private AccountRepository accounts;
+
+    @DisplayName("안읽은 알림 목록 조회")
+    @Test
+    @WithAccount("user1")
+    void notificationUnreadListView() throws Exception {
+        Account user1 = accounts.findByNickname("user1").orElseThrow();
+        notificationFactory.createNotification(user1, "noti1");
+        notificationFactory.createNotification(user1, "noti2");
+        notificationFactory.createNotification(user1, "noti3");
+        notificationFactory.createNotification(user1, "noti4");
+
+        List<Notification> unreadList = notifications.findAllByToAndCheckedOrderByCreatedAt(user1, false);
+        assertEquals(4, unreadList.size());
+
+        this.mockMvc.perform(get(NotificationController.NOTIFICATION_URL + "/unread"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notification/view"))
+                .andExpect(model().attributeExists("notificationList"))
+                .andExpect(model().attributeExists("unreadCount"))
+                .andExpect(model().attributeExists("readCount"))
+                .andExpect(model().attributeExists("studyCreatedNotificationList"))
+                .andExpect(model().attributeExists("studyUpdatedNotificationList"))
+                .andExpect(model().attributeExists("eventEnrollmentNotificationList"));
+
+        List<Notification> readList = notifications.findAllByToAndCheckedOrderByCreatedAt(user1, true);
+        assertEquals(4, readList.size());
+    }
+
+    @DisplayName("읽은 알림 목록 조회")
+    @Test
+    @WithAccount("user1")
+    void notificationReadListView() throws Exception {
+        this.mockMvc.perform(get(NotificationController.NOTIFICATION_URL + "/read"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notification/view"))
+                .andExpect(model().attributeExists("notificationList"))
+                .andExpect(model().attributeExists("unreadCount"))
+                .andExpect(model().attributeExists("readCount"))
+                .andExpect(model().attributeExists("studyCreatedNotificationList"))
+                .andExpect(model().attributeExists("studyUpdatedNotificationList"))
+                .andExpect(model().attributeExists("eventEnrollmentNotificationList"));
+    }
+}
