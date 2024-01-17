7bab710ef3f1d1365eab9fba465690a2c48adf01
r2soft <paulhana6006@gmail.com>
2024-01-17 10:41:16 +0900

    Add error page

--- src/main/java/me/kktrkkt/studyolle/infra/config/GlobalControllerAdvice.java
+++ src/main/java/me/kktrkkt/studyolle/infra/config/GlobalControllerAdvice.java
@@ -1,14 +1,28 @@
 package me.kktrkkt.studyolle.infra.config;
 
+import lombok.extern.slf4j.Slf4j;
+import me.kktrkkt.studyolle.modules.account.CurrentUser;
+import me.kktrkkt.studyolle.modules.account.entity.Account;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> defaultException() {
-        return ResponseEntity.badRequest().build();
+    @ExceptionHandler
+    public String defaultException(@CurrentUser Account account, HttpServletRequest request, RuntimeException e) {
+        if(account != null) {
+            log.info("'{}' requested '{}'", account.getNickname(), request.getRequestURL());
+        }
+        else {
+            log.info("'{}' requested '{}'", request.getRemoteAddr(), request.getRequestURL());
+        }
+
+        log.error("bad request", e);
+        return "error";
     }
 }
